5f7e797a34989947f89019e53cd6d7f5f341cece
wazeerc <58399263+wazeerc@users.noreply.github.com>
2023-02-09 16:57:42 +0400

    added more prompts

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
  *05/02 - formatted output and refactored Run function
  *08/02 - added promts and refactored async functions
  *09/02 - added details to promts
+ *09/02 - added more prompts
  *
  *Logs (<developer's_initial>);
  *
@@ -32,7 +33,7 @@ import { createInterface } from "readline";
 const dataset =
   "https://raw.githubusercontent.com/MrSunshyne/mauritius-dataset-electricity/main/data/power-outages.json"; /* CEB dataset in JSON format by MrSunshyne 
   - https://github.com/MrSunshyne/ */
-let data, region;
+let data, region, amt_po;
 const districts = [
   "blackriver",
   "flacq",
@@ -57,24 +58,50 @@ const readline = createInterface({
 const readLineAsync = (msg) => {
   return new Promise((resolve) => {
     readline.question(msg, (userRes) => {
-      //exit if user types "exit"
       if (userRes === "exit") process.exit(0);
+      //remove spaces and convert to lowercase
+      userRes = userRes.replace(/\s/g, "").toLowerCase();
       //if user input is not a known district, ask again
       if (!districts.includes(userRes)) {
-        console.log(`\n❌ District '${userRes}' not found. Try again.\n`);
-        resolve(readLineAsync(msg));
+        //check if user input made a typo
+        let typo = districts.find((district) => {
+          return district.includes(userRes);
+        });
+        if (typo) {
+          console.log(`\n❌ Did you mean '${typo}'? Try again.\n`);
+          resolve(readLineAsync(msg));
+        } else {
+          console.log(`\n❌ District not found. Try again.\n`);
+          resolve(readLineAsync(msg));
+        }
       }
       resolve(userRes);
     });
   });
 };
 
-//get user input and return as string
+//create promise to get user input for number of power outages to display
+const readLineAsync2 = (msg) => {
+  return new Promise((resolve) => {
+    readline.question(msg, (userRes) => {
+      if (userRes === "exit") process.exit(0);
+      //ask for number of power outages to display
+      if (isNaN(userRes)) {
+        console.log(`\n❌ Please enter a number. Try again.\n`);
+        resolve(readLineAsync2(msg));
+      }
+      resolve(userRes);
+    });
+  });
+};
+
 let get_region = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 500));
   console.log(
-    `\n🗺️ Available districts: Black River, Flacq, Grand Port, Moka, Pamplemousses, Plaines Wilhems, Port Louis, Riviere du Rempart, and Savanne.\n`
+    `\n🗺️ Available districts: Black River, Flacq, Grand Port, Moka, Pamplemousses,Plaines Wilhems, Port Louis, Riviere du Rempart, and Savanne.\n`
   );
-  region = await readLineAsync(`\n🔍 Select a district (without spaces): `);
+  region = await readLineAsync(`\n🔍 Select a district: `);
+  amt_po = await readLineAsync2(`\n🔍 How many power outages to display? `);
   readline.close();
   return region;
 };
@@ -121,7 +148,7 @@ let get_region_po = async (_data) => {
       }; \n`
     );
     //output data for the user defined region (3 upcomming power outages);
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < amt_po; i++) {
       console.log(`\n${i + 1}. 📅 ${_data[region][i].date}
         \n🏙️ ${_data[region][i].locality}, ${_data[region][
         i
@@ -150,11 +177,28 @@ let test_server = () => {
   }
 };
 
+//function to show user a guide on how to use the app
+let user_guide = () => {
+  try {
+    console.log(
+      `Hello there! I am a power outage app for Mauritius 🇲🇺.\n\n📚 User guide:
+        \n1. Select a district from the list.
+        \n2. The app will display the next 3 power outages for that district.
+        \n3. To exit the app, type "exit" and or "ctrl + c".
+        \n4. To view the dataset, visit http://localhost:${PORT}/dataset.
+        `
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 /*
  *RUN SERVER;
  */
 let run = () => {
   try {
+    user_guide();
     //test_server(); /* uncomment to view server details */
     //get user input then fetch data and get power outage data for that region;
     get_region().then(() => fetch_data().then((data) => get_region_po(data)));
